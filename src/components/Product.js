5c24b6500ae747362cddc1c838da677adafbd8ac
sayedsafwet77 <sayedsafwet4@gmail.com>
2022-10-23 21:38:10 +0200

    add card logic

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,21 +1,63 @@
+import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 function Product(props) {
+  const [user_card, setUserCard] = useState([]);
+  const [added_to_card, setAddedToCard] = useState(0);
+  useEffect(() => {
+    let user_card = localStorage.getItem("user_card");
+    if (!user_card) {
+      user_card = [];
+    } else {
+      user_card = JSON.parse(user_card);
+    }
+    setUserCard(user_card);
+    let id_exist = user_card.filter((card) => card.id == props.product.id);
+    if (id_exist.length) {
+      setAddedToCard(1);
+    }
+  }, []);
+
+  function addToCard() {
+    let id_exist = user_card.filter((card) => card.id == props.product.id);
+    if (id_exist.length) {
+      alert("item already added to card");
+    } else {
+      user_card.push({
+        [props.product.id]: { id: props.product.id, amount: 1 },
+      });
+      setUserCard(user_card);
+      setAddedToCard(1);
+
+      localStorage.setItem("user_card", JSON.stringify(user_card));
+    }
+  }
   return (
     <div className="pro">
-      <img className="product-image" src={props.product.pic} />
-      <div className="des">
-        <span>{props.product.name}</span>
-        <h5>{props.product.description}</h5>
-        <div className="stars">
-          <i className="fas fa-star"></i>
-          <i className="fas fa-star"></i>
-          <i className="fas fa-star"></i>
-          <i className="fas fa-star"></i>
-          <i className="fas fa-star"></i>
+      <NavLink
+        key={props.product._id}
+        to={"/product-details/" + props.product._id}
+      >
+        <img className="product-image" src={props.product.pic} />
+        <div className="des">
+          <span>{props.product.name}</span>
+          <h5>{props.product.description}</h5>
+          <div className="stars">
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+          </div>
+          <h4>${props.product.price}</h4>
         </div>
-        <h4>${props.product.price}</h4>
-      </div>
-      <a href="#">
-        <i className="fa fa-cart-shopping cart"></i>
+      </NavLink>
+      <a>
+        <i
+          onClick={addToCard}
+          className={`fa fa-cart-shopping cart ${
+            added_to_card ? "added-to-card" : ""
+          }`}
+        ></i>
       </a>
     </div>
   );
